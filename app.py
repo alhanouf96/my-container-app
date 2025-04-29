6fd59e934cb6643454a836dff56e66826604736b
allhanouf96 <allhanouf96@gmail.com>
2025-04-30 01:02:44 +0300

    Initial two  commit

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure Container Apps!"
+    return "update, Azure Container Apps!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
